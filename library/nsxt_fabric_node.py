e9008d838a1f13541656e994fa4c4806d93fb29f
daxen79 <yasensim@gmail.com>
2018-03-22 14:28:31 +0100

    Add fabric node module

---
+++ library/nsxt_fabric_node.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# Copyright © 2018 VMware, Inc. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions
+# of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+# TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+__author__ = 'yasensim'
+
+
+import requests, time
+try:
+    from com.vmware.nsx.fabric.nodes_client import Status
+    from com.vmware.nsx.fabric_client import Nodes
+    from com.vmware.nsx.model_client import Node
+    from com.vmware.nsx.model_client import HostNodeLoginCredential
+    from com.vmware.nsx.model_client import HostNode
+    from com.vmware.vapi.std.errors_client import NotFound
+    from vmware.vapi.lib import connect
+    from vmware.vapi.security.user_password import \
+        create_user_password_security_context
+    from vmware.vapi.stdlib.client.factories import StubConfigurationFactory
+    from com.vmware.nsx.model_client import ApiError
+    from com.vmware.vapi.std.errors_client import Error
+    HAS_PYNSXT = True
+except ImportError:
+    HAS_PYNSXT = False
+
+
+def listNodes(module, stub_config):
+    try:
+        fabricnodes_svc = Nodes(stub_config)
+    except Error as ex:
+        api_error = ex.data.convert_to(ApiError)
+        module.fail_json(msg='API Error listing nodes: %s'%(api_error.error_message))
+    return fabricnodes_svc.list()
+
+
+def createNode(module, os_type, os_ver, stub_config):
+    ip_addr = []
+    ip_addr.append(module.params['ip_address'])
+    fnodes_svc = Nodes(stub_config)
+    newNode = HostNode(
+	display_name=module.params['display_name'],
+	ip_addresses=ip_addr,
+	os_type=os_type,
+	os_version=os_ver,
+	host_credential=HostNodeLoginCredential(
+            username=module.params['node_username'], 
+            password=module.params['node_passwd'], 
+            thumbprint=module.params['thumbprint']
+        )
+    )
+    try:
+        fnodes_svc.create(newNode)
+        time.sleep(20)
+        resultNode = getNodeByName(module, stub_config)
+        status_svc = Status(stub_config)
+        while True:
+            fn_status = status_svc.get(resultNode.id)
+            if fn_status.host_node_deployment_status == "INSTALL_IN_PROGRESS":
+                time.sleep(10)
+            elif fn_status.host_node_deployment_status == "INSTALL_SUCCESSFUL":
+                time.sleep(5)
+                return resultNode
+            else:
+                module.fail_json(msg='Error in Node status: %s'%(str(fn_status)))
+    except Error as ex:
+        api_error = ex.data.convert_to(ApiError)
+        module.fail_json(msg='API Error creating node: %s'%(api_error.error_message))
+
+
+def deleteNode(module, node, stub_config):
+    fnodes_svc = Nodes(stub_config)
+    node_id = node.id
+    node_name = node.display_name
+    try:
+        fnodes_svc.delete(node_id)
+    except Error as ex:
+        api_error = ex.data.convert_to(ApiError)
+        module.exit_json(changed=False, object_id=node_id, object_name=node_name, message=api_error)
+
+        module.fail_json(msg='API Error Deleting node: %s'%(api_error.error_message))
+    status_svc = Status(stub_config)
+    while True:
+        try:
+            fn_status = status_svc.get(node_id)
+            time.sleep(10)
+        except Error as ex:
+            module.exit_json(changed=True, object_id=node_id, object_name=node_name)
+
+
+def getNodeByName(module, stub_config):
+    result = listNodes(module, stub_config)
+    for vs in result.results:
+        fn = vs.convert_to(Node)
+        if fn.display_name == module.params['display_name']:
+            return fn
+    return None
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            display_name=dict(required=True, type='str'),
+            ip_address=dict(required=True, type='str'),
+            node_username=dict(required=False, type='str'),
+            node_passwd=dict(required=False, type='str', no_log=True),
+            thumbprint=dict(required=False, type='str', no_log=True),
+            state=dict(required=False, type='str', default="present"),
+            nsx_manager=dict(required=True, type='str'),
+            nsx_username=dict(required=True, type='str'),
+            nsx_passwd=dict(required=True, type='str', no_log=True)
+        ),
+        supports_check_mode=False
+    )
+
+    if not HAS_PYNSXT:
+        module.fail_json(msg='pynsxt is required for this module')
+    session = requests.session()
+    session.verify = False
+    nsx_url = 'https://%s:%s' % (module.params['nsx_manager'], 443)
+    connector = connect.get_requests_connector(
+        session=session, msg_protocol='rest', url=nsx_url)
+    stub_config = StubConfigurationFactory.new_std_configuration(connector)
+    security_context = create_user_password_security_context(module.params["nsx_username"], module.params["nsx_passwd"])
+    connector.set_security_context(security_context)
+    requests.packages.urllib3.disable_warnings()
+    if module.params['state'] == "present":
+        node = getNodeByName(module, stub_config)
+        if node is None:
+            result = createNode(module, os_type, os_version, stub_config)
+            module.exit_json(changed=True, object_name=module.params['display_name'], body=str(result))
+        else:
+            module.exit_json(changed=False, object_name=module.params['display_name'], message="Node with name %s already exists!"%(module.params['display_name']))
+
+    elif module.params['state'] == "absent":
+        node = getNodeByName(module, stub_config)
+        if node is None:
+            module.exit_json(changed=False, object_name=module.params['display_name'], message="No Node with name %s"%(module.params['display_name']))
+        else:
+            deleteNode(module, node, stub_config)
+
+
+
+
+
+from ansible.module_utils.basic import *
+
+if __name__ == "__main__":
+    main()
