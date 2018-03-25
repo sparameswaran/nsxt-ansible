b7e51c0d0363c08a75d168dc2c0cbecffdc1d09c
daxen79 <yasensim@gmail.com>
2018-03-25 14:15:11 +0200

    split T0 and T1 in separate files

--- library/nsxt_t1_logical_router.py
+++ library/nsxt_t1_logical_router.py
@@ -25,6 +25,11 @@ try:
     from com.vmware.nsx.model_client import Tag
     from com.vmware.nsx.model_client import LogicalRouter
     from com.vmware.nsx_client import LogicalRouters
+    from com.vmware.nsx.model_client import LogicalRouterLinkPortOnTIER1
+    from com.vmware.nsx.model_client import LogicalRouterLinkPortOnTIER0
+    from com.vmware.nsx.model_client import ResourceReference
+    from com.vmware.nsx_client import LogicalRouterPorts
+    from com.vmware.nsx.model_client import LogicalRouterPort
 
     from com.vmware.vapi.std.errors_client import NotFound
     from vmware.vapi.lib import connect
@@ -55,6 +60,74 @@ def getLogicalRouterByName(module, stub_config):
             return lr
     return None
 
+
+
+def connectT0(t1, module, stub_config):
+    t0port = None
+    lrp_svc = LogicalRouterPorts(stub_config)
+
+    t0_lrp=LogicalRouterLinkPortOnTIER0(
+        display_name="t0-downlink-to_%s"%(t1.display_name), 
+        logical_router_id=module.params['connected_t0_id'], 
+        description=t1.id
+    )
+    try:
+        port = lrp_svc.create(t0_lrp)
+        t0port = port.convert_to(LogicalRouterLinkPortOnTIER0)
+    except Error as ex:
+        api_error = ex.date.convert_to(ApiError)
+        module.fail_json(msg='API Error creating T0 port: %s'%(api_error.error_message))
+
+    t1_lrp=LogicalRouterLinkPortOnTIER1(
+        display_name="%s-uplinklink-to_t0"%(t1.display_name), 
+        description=module.params['connected_t0_id'], 
+        logical_router_id=t1.id,
+        linked_logical_router_port_id=ResourceReference(target_id=t0port.id)
+    )
+    try:
+        t1port = lrp_svc.create(t1_lrp)
+    except Error as ex:
+        api_error = ex.date.convert_to(ApiError)
+        module.fail_json(msg='API Error creating T1 port: %s'%(api_error.error_message))
+
+    return True
+
+def deleteAllPortsOnRouter(lr, module, stub_config):
+    lrp_svc = LogicalRouterPorts(stub_config)
+    lrpList = lrp_svc.list(logical_router_id=lr.id)
+    if lrpList.results:
+        for vs in lrpList.results:
+            lrp = vs.convert_to(LogicalRouterPort)
+            lrp_svc.delete(lrp.id, force=True)
+
+def compareLrpT0T1(lr, module, stub_config):
+    changed = False
+    t0id = None
+    lrp_svc = LogicalRouterPorts(stub_config)
+    lrpList = lrp_svc.list(logical_router_id=lr.id, resource_type='LogicalRouterLinkPortOnTIER1')
+    if lrpList.results:
+        lrp = lrpList.results[0].convert_to(LogicalRouterLinkPortOnTIER1)
+        t0port_id = lrp.linked_logical_router_port_id.target_id
+        t1port_id = lrp.id
+        t0tmp = lrp_svc.get(t0port_id)
+        t0lrp = t0tmp.convert_to(LogicalRouterLinkPortOnTIER0)
+        t0id = t0lrp.logical_router_id
+    if t0id:
+        if t0id != module.params['connected_t0_id']:
+            changed = True
+            if t1port_id:
+                lrp_svc.delete(t1port_id, force=True)
+                lrp_svc.delete(t0port_id, force=True)
+            if module.params['connected_t0_id']:
+                changed = connectT0(lr, module, stub_config)
+    elif not t0id:
+        if t0id != module.params['connected_t0_id']:
+            changed = True
+            if module.params['connected_t0_id']:
+                changed = connectT0(lr, module, stub_config)
+
+    return changed
+
 def main():
     module = AnsibleModule(
         argument_spec=dict(
@@ -62,7 +135,7 @@ def main():
             description=dict(required=False, type='str', default=None),
             failover_mode=dict(required=False, type='str', default=None, choices=['NON_PREEMPTIVE', 'PREEMPTIVE']),
             edge_cluster_id=dict(required=False, type='str', default=None),
-            router_type=dict(required=False, type='str', default='TIER1', choices=['TIER1', 'TIER0']),
+            connected_t0_id=dict(required=False, type='str', default=None),
             high_availability_mode=dict(required=False, type='str', default='ACTIVE_STANDBY', choices=['ACTIVE_STANDBY', 'ACTIVE_ACTIVE']),
             tags=dict(required=False, type='dict', default=None),
             state=dict(required=False, type='str', default="present", choices=['present', 'absent']),
@@ -99,25 +172,43 @@ def main():
                 description=module.params['description'],
                 failover_mode=module.params['failover_mode'],
                 edge_cluster_id=module.params['edge_cluster_id'],
-                router_type=module.params['router_type'],
+                router_type='TIER1',
                 high_availability_mode=module.params['high_availability_mode'],
                 tags=tags
             )
             try:
                 new_lr = lr_svc.create(new_lr)
-                module.exit_json(changed=True, object_name=module.params['display_name'], id=new_lr.id, message="Logical Router with name %s created!"%(module.params['display_name']))
+                mylr = getLogicalRouterByName(module, stub_config)
+                if module.params['connected_t0_id']:
+                    connectT0(mylr, module, stub_config)
+                module.exit_json(changed=True, object_name=module.params['display_name'], id=mylr.id, message="Logical Router with name %s created!"%(module.params['display_name']))
             except Error as ex:
                 module.fail_json(msg='API Error listing Logical Routers: %s'%(str(ex)))
         elif lr:
+            changed = False
             if tags != lr.tags:
+                changed = True
                 lr.tags=tags
+            if module.params['edge_cluster_id'] != lr.edge_cluster_id:
+                lr.edge_cluster_id=module.params['edge_cluster_id']
+                changed = True
+            if changed:
                 new_lr = lr_svc.update(lr.id, lr)
-                module.exit_json(changed=True, object_name=module.params['display_name'], id=new_lr.id, message="Logical Router with name %s has changed tags!"%(module.params['display_name']))
+                module.exit_json(changed=True, object_name=module.params['display_name'], id=new_lr.id, message="Logical Router with name %s has been modified!"%(module.params['display_name']))
+            if compareLrpT0T1(lr, module, stub_config):
+                module.exit_json(changed=True, object_name=module.params['display_name'], id=lr.id, message="Logical Router uplink on T1 with name %s has been modified!"%(module.params['display_name']))
             module.exit_json(changed=False, object_name=module.params['display_name'], id=lr.id, message="Logical Router with name %s already exists!"%(module.params['display_name']))
 
     elif module.params['state'] == "absent":
         if lr:
-            lr_svc.delete(lr.id)
+            try:
+                deleteAllPortsOnRouter(lr, module, stub_config)
+                lr_svc.delete(lr.id)
+            except Error as ex:
+                api_error = ex.date.convert_to(ApiError)
+                module.fail_json(msg='API Error deleting Logical Routers: %s'%(api_error.error_message))
+
+
             module.exit_json(changed=True, object_name=module.params['display_name'], message="Logical Router with name %s deleted!"%(module.params['display_name']))
         module.exit_json(changed=False, object_name=module.params['display_name'], message="Logical Router with name %s does not exist!"%(module.params['display_name']))
 
